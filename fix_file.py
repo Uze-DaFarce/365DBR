3e0947ddb786f2eef03bc0a45415aff2043de769
Uze-DaFarce <uze.dafarce@gmail.com>
2026-03-31 17:21:33 -0600

    Housekeeping: remove patches from root of repo 	deleted:    fix_bg.py 	deleted:    fix_file.py 	deleted:    fix_file2.py 	deleted:    fix_m_main.py 	deleted:    patch_bg.py 	deleted:    patch_collect.py 	deleted:    patch_score_cleanup.py 	deleted:    patch_score_cleanup_desktop.py 	deleted:    replace.patch 	deleted:    revert_bg.py

--- fix_file.py
+++
@@ -1,8 +0,0 @@
-with open('./apps/HeIsRisen/m/main.js', 'r') as f:
-    lines = f.readlines()
-
-with open('./apps/HeIsRisen/m/main.js', 'w') as f:
-    for line in lines:
-        if "backgroundColor: '#000000'," in line:
-            continue
-        f.write(line)
